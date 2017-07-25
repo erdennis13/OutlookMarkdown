ee0d391fe29a38d98e54ed7047f67b585b388bf2
Ethan Dennis <erdennis13@gmail.com>
2017-07-25 12:49:09 -0500

    Rendering markdown in composition window

--- function-file/function-file.js
+++ function-file/function-file.js
@@ -1,17 +1,48 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
 'use strict';
 
-(function () {
+var item;
 
-  // The initialize function must be run each time a new page is loaded
-  Office.initialize = function (reason) {
-    
-  };
+Office.initialize = function(){
+  item = Office.context.mailbox.item;
 
-  // Add any ui-less function here
+}
 
-})();
+function renderMarkdownQuick(event) {
+  item.body.getTypeAsync(
+    function (result) {
+      if (result.status == Office.AsyncResultStatus.Failed){
+        console.log(result.error.message);
+        event.completed();
+      }
+      else {
+        item.body.getAsync(
+          result.value,
+          {asyncContext:"This is passed to the callback"},
+          function callback(text) {
+            if (text.value) {
+              var bodyHtml = /<body.*?>([\s\S]*)<\/body>/.exec(text.value)[1];
+              var bodyText = $(bodyHtml).text();
+
+              marked.sanitize = true;
+              marked.smartLists = true;
+              var html = marked(bodyText);
+              
+              item.body.prependAsync(
+                html,
+                { coercionType: result.value,
+                asyncContext: {var3:1, var4:2} },
+                function (asyncResult) {
+                  if (asyncResult.status == 
+                      Office.AsyncResultStatus.Failed) {
+                        console.log(asyncResult.error.message);
+                        event.completed();
+                  }
+                  else {
+                    event.completed();
+                  }
+              });
+            }
+          });
+      }
+  });
+}
--- msg-compose/render-doc.js
+++ msg-compose/render-doc.js
@@ -1,9 +1,52 @@
-(function(){
-  'use strict';
+var item;
 
-  var config;
-  var settingsDialog;
+Office.initialize = function(){
+  item = Office.context.mailbox.item;
 
-  Office.initialize = function(reason){
-  }
-}());
+  $(document).ready(function () {
+    setItemBody();
+  });
+}
+
+function setItemBody() {
+  item.body.getTypeAsync(
+    function (result) {
+      if (result.status == Office.AsyncResultStatus.Failed){
+        write (result.error.message);
+      }
+      else {
+        item.body.getAsync(
+          result.value,
+          {asyncContext:"This is passed to the callback"},
+          function callback(text) {
+            if (text.value) {
+              var bodyHtml = /<body.*?>([\s\S]*)<\/body>/.exec(text.value)[1];
+              var bodyText = $(bodyHtml).text();
+
+              marked.sanitize = true;
+              marked.smartLists = true;
+              var html = marked(bodyText);
+              
+              item.body.prependAsync(
+                html,
+                { coercionType: result.value,
+                asyncContext: {var3:1, var4:2} },
+                function (asyncResult) {
+                  if (asyncResult.status == 
+                      Office.AsyncResultStatus.Failed) {
+                        write(asyncResult.error.message);
+                  }
+                  else {
+                    write(html);
+                  }
+              });
+            }
+
+          });
+      }
+  });
+}
+
+function write(message) {
+  document.getElementById('message').innerText += message;
+}
